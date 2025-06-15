4d02c37249558d17709f8507900e3d50c6a8e1de
M Mohammad Anas <mohammad.anas51@hotmail.com>
2025-06-15 13:07:47 +0530

    Centered the upper nav bar

--- portfolio/src/mainPage.jsx
+++ portfolio/src/mainPage.jsx
@@ -2,13 +2,13 @@ const MainPage = () => {
     return (
         <>
            <div className="mainParent">
-            <div className="navBar flex">
-            <ul className = "">
-                <li>Resume</li>
-                <li>LinkedIn</li>
-                <li>Email</li>
-                <li>GitHub</li>
-                <li>Twitter</li>
+            <div className="navBar w-80 ml-155">
+            <ul className = "flex justify-around mt-6 cursor-pointer ">
+                <li className = "transform transition duration-300 hover:-translate-y-1 hover:scale-105 hover:shadow-xl">LinkedIn</li>
+                <li className = "transform transition duration-300 hover:-translate-y-1 hover:scale-105 hover:shadow-xl">Email</li>
+                <li className = "transform transition duration-300 hover:-translate-y-1 hover:scale-105 hover:shadow-xl">GitHub</li>
+                <li className = "transform transition duration-300 hover:-translate-y-1 hover:scale-105 hover:shadow-xl">Twitter</li> 
+                <li className = "transform transition duration-300 hover:-translate-y-1 hover:scale-105 hover:shadow-xl">Resume</li>
             </ul>
             </div>
            </div>
