7678db7674ca5efdf1be8b394ebd2766404271c7
M Mohammad Anas <mohammad.anas51@hotmail.com>
2025-07-15 12:16:00 +0530

    trying to add the project cards

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
+interface Projects{
+  title: string;
+  description: string;
+  liveUrl: string;
+  githubRepo: string;
+}
+
+async function getProjects(): Promise<Projects[]>{
+  const result = await fetch('http://localhost:4000/projects');
+  return result.json() ;
+}
+
+
 function Projects() {
   return (
-    <div className="mt-10 flex justify-center items-center">
+    <div className="mt-10 flex flex-col justify-center items-center">
         <p className="font-bold mr-65">Proof Of Work</p>
+        <p>Cards</p>
    </div>
   )
 }
