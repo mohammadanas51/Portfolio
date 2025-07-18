dc4a3ca1fc823fd607ddaaf9644dbba988b31b24
M Mohammad Anas <mohammad.anas51@hotmail.com>
2025-07-18 14:06:19 +0530

    Added second project

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,36 +1,18 @@
-import { get } from 'http';
-import React from 'react'
-
-interface Projects{
-  title: string;
-  description: string;
-  liveUrl: string;
-  githubRepo: string;
-}
-
-
-
-// async function getProjects(): Promise<Projects[]>{
-//   const result = await fetch('http://localhost:4000/projects');
-//   return result.json() ;
-// }
-
+import React from "react";
 
 async function Projects() {
-  // const projects = await getProjects() ;
-
-
   return (
     <div className="mt-10 flex flex-col justify-center items-center">
       <p className="font-bold mr-65">Proof Of Work</p>
       <div className="cards">
-        <div className="cardOne outline-2 outline-offset-2 outline-blue-500 ">
+
+        <div className="cardOne outline-2 outline-offset-2 outline-blue-500 md:w-150 ml-56 mt-3">
           <div className="titleLink flex justify-between">
             <h1>CommitPay</h1>
             <a
               href="https://commitpay-mvp.web.app/"
               className="text-emerald-300"
-              target='_blank'
+              target="_blank"
             >
               Live Link
             </a>
@@ -55,13 +37,54 @@ async function Projects() {
                 entrepreneurial drive to solve real developer problems through
                 lean, user-focused iteration.
               </li>
-
             </ul>
           </div>
         </div>
+
+        {/* Second Card starts here */}
+
+
+        <div className="cardOne outline-2 outline-offset-2 outline-blue-500 md:w-150 ml-56 mt-3">
+          <div className="titleLink flex justify-between">
+            <h1>CommitPay Waitlist</h1>
+            <a
+              href="https://commit-pay-waitlist.netlify.app/"
+              className="text-emerald-300"
+              target="_blank"
+            >
+              Live Link
+            </a>
+          </div>
+          <p>Description</p>
+          <div className="desc">
+            <ul className="mt-2 list-disc list-inside space-y-2">
+              <li>
+                A bootstrapped SaaS platform that helps open-source maintainers
+                post paid beginner-friendly issues, making it easier for new
+                developers to earn through real-world contributions.
+              </li>
+
+              <li>
+                Launched the MVP using React, Firebase, and Firestore, and grew
+                to 35+ signups with early adopter maintainers listing paid tasks
+                — all within the first few weeks.
+              </li>
+
+              <li>
+                Built with a product-first mindset, CommitPay reflects my
+                entrepreneurial drive to solve real developer problems through
+                lean, user-focused iteration.
+              </li>
+            </ul>
+          </div>
+        </div>
+
+
+
+
       </div>
     </div>
   );
 }
 
-export default Projects
+export default Projects;
