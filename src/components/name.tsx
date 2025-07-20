5e2adda6c0ddd2221233cf4b0eb61daa09e5ebbd
M Mohammad Anas <mohammad.anas51@hotmail.com>
2025-07-20 18:31:36 +0530

    changed the font for my name

--- src/components/name.tsx
+++ src/components/name.tsx
@@ -2,15 +2,22 @@
 
 import React from 'react'
 import { motion } from "framer-motion"
+import { Source_Code_Pro } from 'next/font/google';
+
+const sourceCodeFont = Source_Code_Pro({
+  subsets:["latin"],
+  weight : "400",
+  variable : "--font-sourceCode",
+})
 
 function Name() {
   return (
-    <div className="text-center mt-5 text-4xl">
+    <div className={`text-center mt-5 text-4xl ${sourceCodeFont.variable}`}>
        <motion.button
         initial={{ scale: 0 }}
         animate={{ scale: 1 }}
         transition={{ duration: 0.5 }}
-        className=" text-white"
+        className=" text-white font-main"
       >
         Hi, I'm Mohammad Anas!
       </motion.button> 
