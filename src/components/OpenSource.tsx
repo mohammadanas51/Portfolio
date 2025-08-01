3a3635ccb4233045c4e289445b019e079ab865de
M Mohammad Anas <mohammad.anas51@hotmail.com>
2025-08-01 10:34:25 +0530

    added the open source contributions

--- src/components/OpenSource.tsx
+++ src/components/OpenSource.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import ProjectCard from "./projectCards";
 
 import { JetBrains_Mono } from "next/font/google";
 import { Space_Grotesk } from "next/font/google";
@@ -21,6 +22,37 @@ function OpenSource() {
   return (
     <div className={`${jetBrains.variable} text-center mt-10`}>
       <div className={`font-bold text-3xl font-heading `}> Open Source Contributions</div>
+      <div className="cards">
+        <ProjectCard
+          title="freeCodeCamp"
+          link="https://github.com/freeCodeCamp/contribute/pull/743"
+          descriptionPoints={[
+            "Added hover effects to the cards in the landing page.",
+          ]}
+        />
+
+        <ProjectCard
+          title="Kestra-io"
+          link="https://github.com/kestra-io/plugin-notifications/pull/243"
+          descriptionPoints={[
+            "Fixed the Slack icon's padding to make it consistent across pages.",
+          ]}
+        />
+        <ProjectCard
+          title="Prepnerdz"
+          link="https://github.com/Shubhashish-Chakraborty/prepnerdz/pull/42"
+          descriptionPoints={[
+            "Added a testimonial page to their platform.",
+          ]}
+        />
+        <ProjectCard
+          title="Prepnerdz"
+          link="https://github.com/Shubhashish-Chakraborty/prepnerdz/pull/47"
+          descriptionPoints={[
+            "Redesigned the 404 Not Found Page.",
+          ]}
+        />
+      </div>
     </div>
   );
 }
